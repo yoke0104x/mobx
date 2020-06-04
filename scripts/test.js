be7d25e39063ee0b99c1d3d5546fb8c702cf04ee
dingmeng <m17633465271@163.com>
2020-06-04 18:11:17 +0800

    red提交的

--- scripts/test.js
+++ scripts/test.js
@@ -2,12 +2,18 @@
  * @Author: dingmeng
  * @Date: 2020-06-04 17:59:57
  * @LastEditors: dingmeng
- * @LastEditTime: 2020-06-04 18:01:13
+ * @LastEditTime: 2020-06-04 18:10:22
  * @FilePath: \新建文件夹\scripts\test.js
  * @name: 
  */ 
 'use strict';
 
+
+
+
+
+
+console.log("这是red分支上面的东西")
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
